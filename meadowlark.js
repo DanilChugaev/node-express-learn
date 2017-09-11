fcff2cfae43a8b59b8e9e013b11591b35cfbbf37
Danil <danilchugaev@mail.ru>
2017-09-11 20:08:57 +0500

    добавил маршрутизацию и вьюшки

--- meadowlark.js
+++ meadowlark.js
@@ -1,31 +1,35 @@
-var express = require('express'),
+const 	express    = require('express'),
+		app        = express(),
+		handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 
-	app     = express();
+var 	fortune	   = require('./lib/fortune.js');
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 7000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req,res) {
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
-
 app.get('/about', function (req,res) {
-	res.type('text/plain');
-	res.send('o meadowlark');
+    res.render('about',{
+    	fortune: fortune.getFortune()
+	});
 });
-
 app.use(function (req,res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - не найдено');
+    res.render('404');
 });
-
 app.use(function (err, req, res, next) {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - ошибка сервера');
+    res.render('500');
 });
+
 
 app.listen(app.get('port'), function () {
 	console.log('Express запущен на http://localhost:' + app.get('port') + '; Нажмите ctrl+c для завершения')
