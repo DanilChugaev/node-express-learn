f30afe5284aff6e4988d52dd403c5b064cc7a434
Danil <danilchugaev@mail.ru>
2017-10-15 21:31:02 +0500

    добавил частичный шаблон

--- meadowlark.js
+++ meadowlark.js
@@ -1,9 +1,48 @@
 const 	express    = require('express'),
-		app        = express(),
-		handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
+		app        = express();
 
-var 	fortune	   = require('./lib/fortune.js');
 
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
+var fortune = require('./lib/fortune.js');
+
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portlend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность',
+                temp: '54.1 F (12.3 C)'
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность',
+                temp: '54.1 F (12.3 C)'
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность',
+                temp: '54.1 F (12.3 C)'
+            }
+
+        ]
+    }
+}
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -13,6 +52,11 @@ app.set('port', process.env.PORT || 7000);
 app.use(function (req, res, next) {
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
+});
+app.use(function (req,res,next) {
+    if(!res.locals.pertials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -53,6 +97,7 @@ app.use(function (err, req, res, next) {
 	res.status(500);
     res.render('500');
 });
+
 
 
 app.listen(app.get('port'), function () {
